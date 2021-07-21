011178b04d900fa5397070d8d21b8365422614a4
Beer-and-Skittles <jessicahu1012@gmail.com>
2021-07-21 17:37:03 +0800

    maze_game.py updated
    game first draft completed

---
+++ maze_game.py
@@ -0,0 +1,137 @@
+import maze_dfs as dfs
+import pygame as pg
+import random as rnd
+import time
+
+
+screen_h = 800
+screen_w = 800
+margin_x = 70
+margin_y = 70
+maze_row = 15
+maze_col = 15
+sp_x = (screen_w - 2*margin_x)/maze_col
+sp_y = (screen_h - 2*margin_y)/maze_row
+wall_w   = 3
+unit_len = 2
+plr_sz   = 10
+
+bg_clr   = (0, 36, 81)      # muted indigo
+plr_clr  =   (156, 36, 81)  # wine red
+maze_clr = [(235, 187, 255), (255, 197, 143), (255, 238, 164), (115, 201, 145)]
+# lavender, pastel orange, pastel yellowm, avocado
+
+def drawMaze(dfs_maze, screen):     # draws maze
+
+    for r in range(maze_row):
+        for c in range(maze_col):
+
+            crnt_cell = dfs_maze.maze[r][c]
+            color = maze_clr[int(r/maze_row*len(maze_clr))]
+            x = margin_x + c*sp_x
+            y = margin_y + r*sp_y
+
+            if(crnt_cell.walls[0]): # top wall
+                pg.draw.line(screen, color, (x, y), (x+sp_x, y), wall_w)
+
+            if(crnt_cell.walls[1]): # right wall
+                pg.draw.line(screen, color, (x+sp_x, y), (x+sp_x, y+sp_y), wall_w)
+
+            if(crnt_cell.walls[2]): # bottom wall
+                pg.draw.line(screen, color, (x+sp_x, y+sp_y), (x, y+sp_y), wall_w)
+
+            if(crnt_cell.walls[3]): # left wall
+                pg.draw.line(screen, color, (x, y+sp_y), (x, y), wall_w)
+
+def inRein(dfs_maze, x, y):     # returns true if the player position is legal
+
+    # if not (x-plr_sz>=margin_x+wall_w and x+plr_sz<=screen_w-margin_x-wall_w\
+    # and y-plr_sz>=margin_y+wall_w and y+plr_sz<=screen_h-margin_y-wall_w):
+    #     return False
+
+    row = int((y - margin_y)/sp_y) 
+    col = int((x - margin_x)/sp_x)
+    crnt_cell = dfs_maze.maze[row][col]
+
+    # top
+    if(y - plr_sz < margin_y + row*sp_y and crnt_cell.walls[0]):
+        return False
+    # bottom
+    if(y + plr_sz > margin_y + (row+1)*sp_y and crnt_cell.walls[2]):
+        return False
+    # left
+    if(x - plr_sz < margin_x + col*sp_x and crnt_cell.walls[3]):
+        return False
+    # right
+    if(x + plr_sz > margin_x + (col+1)*sp_x and crnt_cell.walls[1]):
+        return False
+    
+    return True
+
+def winGame(x, y):
+    row = int((y - margin_y)/sp_y)
+    col = int((x - margin_x)/sp_x)
+    if(row+1 == maze_row and col+1 == maze_col):
+        return True
+    return False
+
+def main():
+
+    # init screen
+    pg.init(),
+    screen = pg.display.set_mode((screen_w ,screen_h))
+    screen.fill(bg_clr)
+
+    # init maze
+    dfs_maze = dfs.Maze(maze_row, maze_col)
+    dfs_maze.buildMaze()
+    drawMaze(dfs_maze, screen)
+
+    # init player
+    plr_x = margin_x + sp_x/2
+    plr_y = margin_y + sp_y/2
+    pg.draw.circle(screen, plr_clr, (plr_x, plr_y), plr_sz)
+
+    key_pressed = False
+    game = True
+    while(game):
+        time.sleep(0.005)
+        for event in pg.event.get():
+
+            if(event.type == pg.QUIT):
+                game = False
+            
+            if(event.type == pg.KEYUP):
+                key_pressed = False
+
+            if(event.type == pg.KEYDOWN):
+                key_pressed = True
+
+        if(key_pressed):    # a key is pressed
+            if(event.key == pg.K_UP):
+                if(inRein(dfs_maze, plr_x, plr_y-unit_len)):
+                    plr_y -= unit_len
+
+            elif(event.key == pg.K_DOWN):
+                if(inRein(dfs_maze, plr_x, plr_y+unit_len)):
+                    plr_y += unit_len
+
+            elif(event.key == pg.K_RIGHT):
+                if(inRein(dfs_maze, plr_x+unit_len, plr_y)):
+                    plr_x += unit_len
+
+            elif(event.key == pg.K_LEFT):
+                if(inRein(dfs_maze, plr_x-unit_len, plr_y)):
+                    plr_x -= unit_len
+            
+            if(winGame(plr_x, plr_y)):
+                print("WIN GAME!")
+            
+            screen.fill(bg_clr)
+            pg.draw.circle(screen, plr_clr, (plr_x, plr_y), plr_sz)
+
+        drawMaze(dfs_maze, screen)
+        pg.display.update()
+
+if __name__ == '__main__':
+    main()
